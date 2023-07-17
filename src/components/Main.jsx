1a91dfd44bfa3ae4eb660371ebffb127544d4bff
f-aljardan <fayaljardan@gmail.com>
2023-07-17 19:15:28 +0300

    inital commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+
+export default function Main(){
+    return(
+        <div className="container">
+      <div className="h1">
+  Fun facts about React
+        </div>
+
+       
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+        </ul>
+
+        </div>
+  
+    )
+    }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Navbar(){
+return(
+    <nav className="nav">
+        <img alt="react" src="src\assets\react.png" width="70"/>
+        <h2> React Facts</h2>
+        <div className="topic">
+            <h3>React Course -</h3>
+            <h3>project 1</h3>
+        </div>
+    </nav>
+)
+} 
